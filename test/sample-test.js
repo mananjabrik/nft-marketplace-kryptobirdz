252be1c48a3cc8bb6f435f29414c1440dc7b19aa
mananjabrik <manan.jabrik@gmail.com>
2022-01-05 19:15:18 +0700

    make readable test hardhat test

--- test/sample-test.js
+++ test/sample-test.js
@@ -31,7 +31,20 @@ describe('KBMarket', function () {
     await market
       .connect(buyerAddress)
       .createMarketSale(nftContractAddress, 1, { value: auctionPrice });
-    const items = await market.fetchMarketTokens();
+    let items = await market.fetchMarketTokens();
+    items = await Promise.all(
+      items.map(async (i) => {
+        const tokenUri = await nft.tokenURI(i.tokenId);
+        let item = {
+          price: i.price.toString(),
+          tokenId: i.tokenId.toString(),
+          seller: i.seller,
+          owner: i.owner,
+          tokenUri,
+        };
+        return item;
+      })
+    );
     console.log('items', items);
   });
 });
