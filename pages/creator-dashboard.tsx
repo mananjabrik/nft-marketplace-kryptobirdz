7c26c14bd92171a921ec0505bf45c435c0d13948
mananjabrik <manan.jabrik@gmail.com>
2022-01-07 21:51:57 +0700

    update design

--- pages/creator-dashboard.tsx
+++ pages/creator-dashboard.tsx
@@ -26,33 +26,35 @@ export default function CreatorDashboard() {
     });
   }, []);
   return (
-    <div>
-      <div className="p-4">
-        {loadingState === true ? (
-          <Loading />
-        ) : (
-          <h2 className="text-2xl py-2">Items Created</h2>
-        )}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {nfts?.map((nft, i) => (
-            <CardItem {...nft} key={i} />
-          ))}
-        </div>
-      </div>
-      <div className="px-4">
-        {loadingState === true ? <Loading /> : null}
-        {sold?.length ? (
-          <div>
-            <h2 className="text-2xl py-2">Items soldout</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-              {sold?.map((nft, i) => (
-                <CardItem {...nft} key={i} />
-              ))}
-            </div>
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: '1600px' }}>
+        <div className="p-4">
+          {loadingState === true ? (
+            <Loading />
+          ) : (
+            <h2 className="text-2xl py-2">Items Created</h2>
+          )}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {nfts?.map((nft, i) => (
+              <CardItem {...nft} key={i} />
+            ))}
           </div>
-        ) : (
-          <h1 className="py-10 px-20 text-3xl">No assets created</h1>
-        )}
+        </div>
+        <div className="px-4">
+          {loadingState === true ? <Loading /> : null}
+          {sold?.length ? (
+            <div>
+              <h2 className="text-2xl py-2">Items soldout</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                {sold?.map((nft, i) => (
+                  <CardItem {...nft} key={i} />
+                ))}
+              </div>
+            </div>
+          ) : (
+            <h1 className="py-10 px-20 text-3xl">No assets created</h1>
+          )}
+        </div>
       </div>
     </div>
   );
